fd5dec0d63ee5a7182c9c86cae0a19fb09453ab6
Samuele Casarin <eleumasc@gmail.com>
2025-01-13 15:41:25 +0100

    Change output format of toSerializableValue

--- setup/util/toSerializableValue.js
+++ setup/util/toSerializableValue.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const Array = require("../safe/Array");
+const WeakMap = require("../safe/WeakMap");
+const getOrCreateMapEntry = require("./getOrCreateMapEntry");
 
 const RegExp = global.RegExp;
 const String = global.String;
@@ -8,10 +10,12 @@ const String = global.String;
 const getPrototypeOf = Reflect.getPrototypeOf;
 const ownKeys = Reflect.ownKeys;
 const getOwnPropertyDescriptor = Reflect.getOwnPropertyDescriptor;
-
 const fromEntries = Object.fromEntries;
 
-function toSerializableValue(value, depth, objectMap) {
+const objectMap = new WeakMap();
+let nextObjectId = 1;
+
+function toSerializableValue(value, depth) {
   switch (typeof value) {
     case "boolean":
     case "bigint":
@@ -20,46 +24,55 @@ function toSerializableValue(value, depth, objectMap) {
     case "undefined":
       return value;
     case "symbol":
-      return { __type__: "symbol", description: value.description };
+      return { type: "symbol", description: value.description };
     case "object": {
       if (!value) {
         return value;
       } else if (value instanceof RegExp) {
-        return { __type__: "regexp", pattern: String(value) };
+        return { type: "RegExp", pattern: String(value) };
       } else if (Array.isArray(value)) {
-        if (depth > 0) {
-          return value.map((element) =>
-            toSerializableValue(element, depth - 1)
-          );
-        } else {
-          return { __type__: "array", length: value.length };
-        }
+        return {
+          type: "Array",
+          id: getObjectId(value),
+          value:
+            depth > 0
+              ? value.map((element) => toSerializableValue(element, depth - 1))
+              : null,
+        };
       } else {
-        if (depth > 0) {
-          return fromEntries([
-            ...ownKeys(value)
-              .filter((key) => typeof key === "string")
-              .map((key) => [key, getOwnPropertyDescriptor(value, key)])
-              .filter(([_, d]) => d && d.enumerable && !d.get && !d.set)
-              .map(([key, d]) => [key, d.value])
-              .map(([key, element]) => {
-                return [key, toSerializableValue(element, depth - 1)];
-              }),
-            ["__constructor__", getConstructorName(value)],
-          ]);
-        } else {
-          return {
-            __type__: "object",
-            __constructor__: getConstructorName(value),
-          };
-        }
+        const constructorName = getConstructorName(value);
+        return {
+          type: "object",
+          id: getObjectId(value),
+          constructor: constructorName !== "Object" ? constructorName : null,
+          value:
+            depth > 0
+              ? fromEntries(
+                  ownKeys(value)
+                    .filter((key) => typeof key === "string")
+                    .map((key) => [key, getOwnPropertyDescriptor(value, key)])
+                    .filter(([_, d]) => d && d.enumerable && !d.get && !d.set)
+                    .map(([key, d]) => [key, d.value])
+                    .map(([key, element]) => {
+                      return [key, toSerializableValue(element, depth - 1)];
+                    })
+                )
+              : null,
+        };
       }
     }
     case "function":
-      return { __type__: "function" };
+      return {
+        type: "function",
+        id: getObjectId(value),
+      };
     default:
-      return { __type__: "unknown" };
+      throw new Error(); // this should never happen
   }
+}
+
+function getObjectId(object) {
+  return getOrCreateMapEntry(objectMap, object, () => nextObjectId++);
 }
 
 function getConstructorName(object) {
