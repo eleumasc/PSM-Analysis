d36e4a871daeb29c7471d62054ee1417419e9ba5
Samuele Casarin <eleumasc@gmail.com>
2025-04-02 15:32:31 +0200

    Fix protected functions in toSerializableValue of setup

--- setup/util/toSerializableValue.js
+++ setup/util/toSerializableValue.js
@@ -1,9 +1,12 @@
 "use strict";
 
-const $WeakMap = require("../safe/WeakMap");
+const WeakMap = require("../safe/WeakMap");
 const getOrCreateMapEntry = require("./getOrCreateMapEntry");
+const unbind = require("./unbind");
 
 const $Array$isArray = Array.isArray;
+const $Array$$filter = unbind(Array.prototype.filter);
+const $Array$$map = unbind(Array.prototype.map);
 const Object$fromEntries = Object.fromEntries;
 const $Reflect$getPrototypeOf = Reflect.getPrototypeOf;
 const $Reflect$ownKeys = Reflect.ownKeys;
@@ -11,7 +14,7 @@ const $Reflect$getOwnPropertyDescriptor = Reflect.getOwnPropertyDescriptor;
 const $RegExp = global.RegExp;
 const $String = global.String;
 
-const objectMap = new $WeakMap();
+const objectMap = new WeakMap();
 let nextObjectId = 1;
 
 function toSerializableValue(value, depth) {
@@ -35,7 +38,9 @@ function toSerializableValue(value, depth) {
           id: getObjectId(value),
           value:
             depth > 0
-              ? value.map((element) => toSerializableValue(element, depth - 1))
+              ? $Array$$map(value, (element) =>
+                  toSerializableValue(element, depth - 1)
+                )
               : null,
         };
       } else {
@@ -47,17 +52,24 @@ function toSerializableValue(value, depth) {
           value:
             depth > 0
               ? Object$fromEntries(
-                  $Reflect$ownKeys(value)
-                    .filter((key) => typeof key === "string")
-                    .map((key) => [
+                  (() => {
+                    let t;
+                    t = $Reflect$ownKeys(value);
+                    t = $Array$$filter(t, (key) => typeof key === "string");
+                    t = $Array$$map(t, (key) => [
                       key,
                       $Reflect$getOwnPropertyDescriptor(value, key),
-                    ])
-                    .filter(([_, d]) => d && d.enumerable && !d.get && !d.set)
-                    .map(([key, d]) => [key, d.value])
-                    .map(([key, element]) => {
+                    ]);
+                    t = $Array$$filter(
+                      t,
+                      ([_, d]) => d && d.enumerable && !d.get && !d.set
+                    );
+                    t = $Array$$map(t, ([key, d]) => [key, d.value]);
+                    t = $Array$$map(t, ([key, element]) => {
                       return [key, toSerializableValue(element, depth - 1)];
-                    })
+                    });
+                    return t;
+                  })()
                 )
               : null,
         };
